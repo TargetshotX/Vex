faf339fa5ae178fe989c880139be164f83aaaf3b
TargetshotX <129337379+TargetshotX@users.noreply.github.com>
2023-07-10 15:38:40 -0700

    Add files via upload

---
+++ OverUnder/315AutonsRight/src/main.cpp
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       varunkaushik                                              */
+/*    Created:      Sat Jul 08 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Drivetrain           drivetrain    1, 2, 3, 4      
+// Intake               motor         5               
+// Catapult             motor         6               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
